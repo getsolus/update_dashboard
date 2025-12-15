3ab2f1c550064e171f5dcdc25a0e53ce8b7b8af1
Hans K <hans@communitycomputing.net>
2025-12-14 23:40:09 -0500

    Initial semi-working sortable table

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ import pathlib
 from rich.progress import track
 import typer
 from rich import print
+from jinja2 import Environment, PackageLoader
+from jinja2_lucide import LucideExtension
 
 app = typer.Typer()
 
+env = Environment(
+    loader=PackageLoader('main', 'templates'),
+    autoescape=True,
+    extensions=[LucideExtension],
+)
 
 @app.command()
-def render_outdated(packages_dir: pathlib.Path):
+def render_outdated(packages_dir: pathlib.Path, output_filename: pathlib.Path = pathlib.Path('./deploy/outdated.html')):
     print("Finding outdated packages using ent...")
     ent_packages = get_outdated(packages_dir)
     print("Done!")
@@ -16,8 +23,10 @@ def render_outdated(packages_dir: pathlib.Path):
         dict(**get_metadata(packages_dir, package['name']), current_version=package.get('current_version'), new_version=package.get('new_version'))
         for package in track(ent_packages, description='Getting metadata for all outdated packages...')
     ]
-    print(outdated_packages)
-
+    template = env.get_template('outdated_packages.html')
+    with output_filename.open('w') as output_file:
+        output_file.write(template.render(outdated_packages=outdated_packages, page_title='Outdated packages', num_outdated=len(outdated_packages)))
+    # print(template.render(outdated_packages=outdated_packages))
 
 if __name__ == '__main__':
     app()
